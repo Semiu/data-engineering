d65881516ca325e0b38c9ff1850a1d83659ab4e6
Semiu Akanmu <ayobami.sm@gmail.com>
2023-02-14 23:30:16 -0600

    next to SQL to SQL ETL

--- cloud-dw/redshift-etl-project/README.md
+++ cloud-dw/redshift-etl-project/README.md
@@ -1,1 +1,14 @@
 
+how to treat NULL value if the source has NULL value and the staging tables won't have NULL value
+
+id expexted to be autoincrement in Redshift.
+
+setting primary and foreign keys for start schema's fact and dimension tables in redshift, and the referential relationship
+
+sketch out the star schema
+
+suitable datatype for day, week, month, year, weekday
+
+Establish the relationship between the columns in the facts and dimension tables with the dimensions in the staging tables
+
+Converting timestamps into start_time, day, week, month, year, weekday, etc
--- cloud-dw/redshift-etl-project/sql_queries.py
+++ cloud-dw/redshift-etl-project/sql_queries.py
@@ -23,26 +23,107 @@ time_table_drop = "DROP TABLE IF EXISTS 'time_table';"
 # CREATE TABLES
 # Staging tables
 staging_events_table_create= ("""
+CREATE TABLE "staging_events_table" (
+    "id" IDENTITY(0,1) INTEGER NOT NULL,
+    "artist" VARCHAR(25),
+    "auth" VARCHAR(10) NOT NULL,
+    "firstName" VARCHAR(19) NOT NULL,
+    "gender" VARCHAR(5) NOT NULL,
+    "itemInSession" INTEGER NOT NULL,
+    "lastName" VARCHAR(15) NOT NULL,
+    "length" DOUBLE PRECISION,
+    "level" VARCHAR(5) NOT NULL,
+    "location" VARCHAR(25) NOT NULL,
+    "method" VARCHAR(5) NOT NULL,
+    "page" VARCHAR(10) NOT NULL,
+    "registration" BIGINT NOT NULL,
+    "sessionid" INTEGER NOT NULL,
+    "song" VARCHAR(100),
+    "status" INTEGER NOT NULL,
+    "ts" TIMESTAMP NOT NULL,
+    "userAgent" VARCHAR(100) NOT NULL,
+    "userId" INTEGER NOT NULL
+);
 """)
 
 staging_songs_table_create = ("""
+CREATE TABLE "staging_songs_table" (
+    "id" IDENTITY(0,1) INTEGER NOT NULL,
+    "num_songs" INTEGER NOT NULL,
+    "artist_id" VARCHAR(25) NOT NULL,
+    "artist_latitude" GEOGRAPHY,
+    "artist_longitude" GEOGRAPHY,
+    "artist_location" GEOGRAPHY,
+    "artist_name" VARCHAR(25) NOT NULL,
+    "song_id" VARCHAR(25) NOT NULL,
+    "title" VARCHAR(25) NOT NULL,
+    "duration" DOUBLE PRECISION,
+    "year" INTEGER NOT NULL
+);
 """)
 
 # Fact table
+# songplay_table
 songplay_table_create = ("""
+CREATE TABLE "songplay_table" (
+    "songplay_id" IDENTITY(0,1) INTEGER NOT NULL,
+    "start_time" TIME NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "level"  VARCHAR(5) NOT NULL,
+    "song_id" VARCHAR(25) NOT NULL,
+    "artist_id" VARCHAR(25) NOT NULL,
+    "session_id" INTEGER NOT NULL,
+    "location" VARCHAR(25) NOT NULL,
+    "user_agent" VARCHAR(100) NOT NULL
+);
 """)
 
 # Dimension tables
+# user table
 user_table_create = ("""
+CREATE TABLE "user_table" (
+    "user_id" INTEGER NOT NULL,
+    "first_name" VARCHAR(19) NOT NULL,
+    "last_name" VARCHAR(15) NOT NULL,
+    "gender" VARCHAR(5) NOT NULL,
+    "level" VARCHAR(5) NOT NULL
+);
 """)
 
+# song_table
 song_table_create = ("""
+CREATE TABLE "song_table" (
+    "song_id" VARCHAR(25) NOT NULL,
+    "title" VARCHAR(25) NOT NULL,
+    "artist_id" VARCHAR(25) NOT NULL,
+    "year"  INTEGER NOT NULL,
+    "duration" DOUBLE PRECISION
+);
 """)
 
+# artist_table
 artist_table_create = ("""
+CREATE TABLE "artist_table" (
+    "artist_id" VARCHAR(25) NOT NULL,
+    "artist_name" VARCHAR(25) NOT NULL,
+    "artist_location" GEOGRAPHY,
+    "artist_latitude" GEOGRAPHY,
+    "artist_longitude" GEOGRAPHY
+);
 """)
 
 time_table_create = ("""
+CREATE TABLE "time_table" (
+    "timestamp_id" TIMESTAMP NOT NULL,
+    "start_time" TIME NOT NULL,
+    "hour" TIME NOT NULL,
+    "day" TIME NOT NULL,
+    "week" TIME NOT NULL,
+    "month" TIME NOT NULL,
+    "year" TIME NOT NULL,
+    "weekday" TIME NOT NULL
+    
+);
 """)
 
 # STAGING TABLES - Copy from the designated s3 bucket to their respective staging tables
@@ -51,6 +132,7 @@ staging_events_copy = ("""copy staging_events_table from '{}' credentials 'aws_i
 
 staging_songs_copy = ("""copy staging_songs_table from '{}' credentials 'aws_iam_role={}' region 'us-west-2';
 """).format(SONG_DATA, ARN)
+
 
 # FINAL TABLES - SQL to SQL ELT, selecting from the staging tables to designated fact and dimension tables
 songplay_table_insert = ("""
@@ -68,8 +150,8 @@ artist_table_insert = ("""
 time_table_insert = ("""
 """)
 
-# QUERY LISTS
 
+# QUERY LISTS
 create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
